59b72f8d20abeccd2fd13a93e80bf035f7721dda
Dan Mohl <dmohl@yahoo.com>
2014-01-13 15:58:40 -0600

    Added MultiProjectTemplate projects.

---
+++ src/DialogTester/Program.fs
@@ -0,0 +1,10 @@
+﻿open System
+open MultiProjectDialog
+
+[<EntryPoint>]
+[<STAThread>]
+let main argv = 
+    let projects = [ "Test1", "MVC 5 and Web API", "fsharp"; "Test2", "Web API", "FSharp"; "Test3", "My Template", "Generic"]
+    let dialog = new TemplateWizardDialog(projects |> List.toSeq)
+    dialog.ShowDialog()
+    0 // return an integer exit code
