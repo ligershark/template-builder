dac9d590dc3af9a1d5c4fb2ef15acb2fdff8c0bf
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2014-07-02 22:18:52 -0700

    updating to start support to remove ProjectExtension

---
+++ src/LigerShark.TemplateBuilder.Tasks/Extensions/Dummy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LigerShark.TemplateBuilder.Tasks.Extensions {
+    // this is here just to ensure that the xdt assemblies are copied to the bin folder
+    public class Dummy {
+        public void DontRemove() {
+            var result = Microsoft.Web.XmlTransform.TransformFlags.ApplyTransformToAllTargetNodes;
+
+            throw new ApplicationException(string.Format("Don't call this method: [{0}]", result));
+        }
+    }
+}
