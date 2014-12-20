17a6c85ddc3148af5897dba8b76362d00fda7311
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2014-12-20 11:55:12 -0800

    minor update to appveyor.ps1

--- appveyor.ps1
+++ appveyor.ps1
@@ -1,7 +1,7 @@
 ﻿if($env:APPVEYOR_REPO_BRANCH -eq "releasetemplatebuilder"){
     .\build.ps1 -publishTemplateBuilderToNuget
 } 
-elseif( -eq "releasefilereplacer"){
+elseif($env:APPVEYOR_REPO_BRANCH -eq "releasefilereplacer"){
     .\build.ps1 -publishFileReplacerToNuget
 }
 elseif($env:APPVEYOR_REPO_BRANCH -eq "releaseallnugetpkg"){
