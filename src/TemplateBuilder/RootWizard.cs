3b5d4f7568e5fb5240761f561233a58054bc9964
Lynx2504 <wrmmroos@gmail.com>
2015-08-14 15:56:32 +0200

    - fix for multi-project template structure

--- src/TemplateBuilder/RootWizard.cs
+++ src/TemplateBuilder/RootWizard.cs
@@ -1,36 +1,215 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using EnvDTE;
+using EnvDTE80;
 using Microsoft.VisualStudio.TemplateWizard;
 
-namespace TemplateBuilder {
-    public class RootWizard : IWizard {
+namespace TemplateBuilder
+{
+    public class RootWizard : IWizard
+    {
+        // Core Solution object
+        private DTE2 _dte2;
+
         // Use to communicate $saferootprojectname$ to ChildWizard     
         public static Dictionary<string, string> GlobalDictionary = new Dictionary<string, string>();
 
         // Add global replacement parameters     
-        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams) {
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
             // Place "$saferootprojectname$ in the global dictionary. 
             // Copy from $safeprojectname$ passed in my root vstemplate         
+
+            //Get DTE2 object to alter solution structure
+            _dte2 = automationObject as DTE2;
+
+            GlobalDictionary["$destinationdirectory$"] = replacementsDictionary["$destinationdirectory$"];
             GlobalDictionary["$saferootprojectname$"] = replacementsDictionary["$safeprojectname$"];
         }
 
-        public bool ShouldAddProjectItem(string filePath) {
+        public bool ShouldAddProjectItem(string filePath)
+        {
             return true;
         }
 
-        public void RunFinished() {
+        public void RunFinished()
+        {
+            //Get all projects in solution
+            var projects = GetProjects();
+            if (projects == null || !projects.Any()) return;
+
+            //Get the projects directory from first project.
+            var projectsDir = Path.GetDirectoryName(Path.GetDirectoryName(projects.First().FullName));
+            if (projectsDir == null) return;
+
+            //Change the projects location.
+            var solutionStructure = projects.Select(AdjustProjectLocation).ToList();
+
+            //Restructure solution
+            foreach (var keyValuePair in solutionStructure.Where(keyValuePair => !string.IsNullOrWhiteSpace(keyValuePair.Value)))
+            {
+                if (keyValuePair.Key != null)
+                {
+                    //If Key is not null, The project needs to be added to a SolutionFolder
+                    var slnDirectory = keyValuePair.Key.Object as SolutionFolder;
+                    if (slnDirectory != null)
+                    {
+                        slnDirectory.AddFromFile(keyValuePair.Value);
+                    }
+                }
+                else
+                {
+                    //Key is null, add project to Solution root.
+                    _dte2.Solution.AddFromFile(keyValuePair.Value);
+                }
+            }
+
+            //Delete the old directory
+            Directory.Delete(projectsDir);
         }
 
-        public void BeforeOpeningFile(ProjectItem projectItem) {
-        }
-
-        public void ProjectItemFinishedGenerating(ProjectItem projectItem) {
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
 
         }
 
-        public void ProjectFinishedGenerating(Project project) {
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
 
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+
+        }
+
+        /// <summary>
+        /// Moves a folder.
+        /// </summary>
+        /// <param name="sourceDirectory">The source folder.</param>
+        /// <param name="destDirectory">The dest folder.</param>
+        private static void MoveDirectory(string sourceDirectory, string destDirectory)
+        {
+            if (!Directory.Exists(destDirectory))
+            {
+                Directory.CreateDirectory(destDirectory);
+            }
+
+            var files = Directory.GetFiles(sourceDirectory);
+            foreach (var file in files)
+            {
+                var name = Path.GetFileName(file);
+                if (name == null) continue;
+
+                var dest = Path.Combine(destDirectory, name);
+                File.Move(file, dest);
+            }
+
+            var folders = Directory.GetDirectories(sourceDirectory);
+            foreach (var folder in folders)
+            {
+                var name = Path.GetFileName(folder);
+                if (name == null) continue;
+
+                var dest = Path.Combine(destDirectory, name);
+                MoveDirectory(folder, dest);
+            }
+
+            Directory.Delete(sourceDirectory);
+        }
+
+        /// <summary>
+        /// Adjusts the project location.
+        /// </summary>
+        /// <param name="project">The project.</param>
+        /// <returns>The solution structure (key: optional SolutionFolder, value: *.csproj path</returns>
+        private KeyValuePair<Project, string> AdjustProjectLocation(Project project)
+        {
+            var projectFullName = project.FullName;
+            var projectName = Path.GetFileName(projectFullName);
+            if (projectName == null) return new KeyValuePair<Project, string>();
+
+            //Get project parent (this is either a SolutionFolder or solution root (null))
+            var parent = project.ParentProjectItem.ContainingProject.Kind == ProjectKinds.vsProjectKindSolutionFolder ? project.ParentProjectItem.ContainingProject : null;
+
+            //Remove the project from the solution
+            _dte2.Solution.Remove(project);
+
+            //Get the old project directory
+            var oldProjectDir = Path.GetDirectoryName(projectFullName);
+
+            //Go to parent directory
+            var solutionDir = Path.GetDirectoryName(Path.GetDirectoryName(oldProjectDir));
+            if (solutionDir == null) return new KeyValuePair<Project, string>();
+
+            //Create new project directory
+            var newProjectDir = Path.Combine(solutionDir, Path.GetFileNameWithoutExtension(projectName));
+            Directory.CreateDirectory(newProjectDir);
+
+            //Move the project content to new directory
+            MoveDirectory(oldProjectDir, newProjectDir);
+
+            return new KeyValuePair<Project, string>(parent, Path.Combine(newProjectDir, projectName));
+        }
+
+        /// <summary>
+        /// Gets the projects in a solution recursively.
+        /// </summary>
+        /// <returns></returns>
+        private IList<Project> GetProjects()
+        {
+            var projects = _dte2.Solution.Projects;
+            var list = new List<Project>();
+            var item = projects.GetEnumerator();
+
+            while (item.MoveNext())
+            {
+                var project = item.Current as Project;
+                if (project == null)
+                {
+                    continue;
+                }
+
+                if (project.Kind == ProjectKinds.vsProjectKindSolutionFolder)
+                {
+                    list.AddRange(GetSolutionFolderProjects(project));
+                }
+                else
+                {
+                    list.Add(project);
+                }
+            }
+            return list;
+        }
+
+        /// <summary>
+        /// Gets the solution folder projects.
+        /// </summary>
+        /// <param name="solutionFolder">The solution folder.</param>
+        /// <returns></returns>
+        private static IEnumerable<Project> GetSolutionFolderProjects(Project solutionFolder)
+        {
+            var list = new List<Project>();
+            for (var i = 1; i <= solutionFolder.ProjectItems.Count; i++)
+            {
+                var subProject = solutionFolder.ProjectItems.Item(i).SubProject;
+                if (subProject == null)
+                {
+                    continue;
+                }
+
+                // If this is another solution folder, do a recursive call, otherwise add
+                if (subProject.Kind == ProjectKinds.vsProjectKindSolutionFolder)
+                {
+                    list.AddRange(GetSolutionFolderProjects(subProject));
+                }
+                else
+                {
+                    list.Add(subProject);
+                }
+            }
+            return list;
         }
     }
 }
