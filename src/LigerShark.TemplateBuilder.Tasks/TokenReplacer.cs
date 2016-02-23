46edf3856e260a8288ef83a48ed087f698698cac
Mike Lorbetske <mike.lorbetske@n3-p.com>
2016-02-22 20:34:11 -0800

    Conditional regions

--- src/LigerShark.TemplateBuilder.Tasks/TokenReplacer.cs
+++ src/LigerShark.TemplateBuilder.Tasks/TokenReplacer.cs
@@ -10,6 +10,7 @@ namespace LigerShark.TemplateBuilder.Tasks
         private readonly Stream _scratch;
         private readonly string _scratchFileName;
         private readonly Stream _source;
+        private static readonly byte[] NoBytes = new byte[0];
 
         public TokenReplacer(Stream source)
             : this(source, CreateScratchStream())
@@ -146,6 +147,67 @@ namespace LigerShark.TemplateBuilder.Tasks
             return modified;
         }
 
+        public static bool ProcessConditionalRegionsInFile(string fileName, string startToken, string endToken, bool isConditionSatisfied)
+        {
+            bool modified;
+
+            using (var file = File.Open(fileName, FileMode.Open, FileAccess.ReadWrite))
+            {
+                using (var replacer = new TokenReplacer(file))
+                {
+                    modified = replacer.ProcessConditionalRegion(startToken, endToken, isConditionSatisfied);
+                }
+
+                file.Flush();
+            }
+
+            return modified;
+        }
+
+        public static bool ProcessConditionalRegionsInFile(string fileName, string token, string replaceWith, int codepage)
+        {
+            return ReplaceInFile(fileName, token, replaceWith, Encoding.GetEncoding(codepage));
+        }
+
+        public static bool ProcessConditionalRegionsInFile(string fileName, string token, string replaceWith, string encodingName)
+        {
+            return ReplaceInFile(fileName, token, replaceWith, Encoding.GetEncoding(encodingName));
+        }
+
+        public static bool ProcessConditionalRegionsInFile(string fileName, string token, string replaceWith, Encoding encoding)
+        {
+            bool modified;
+
+            using (var file = File.Open(fileName, FileMode.Open, FileAccess.ReadWrite))
+            {
+                using (var replacer = new TokenReplacer(file))
+                {
+                    modified = replacer.Replace(token, replaceWith, encoding);
+                }
+
+                file.Flush();
+            }
+
+            return modified;
+        }
+
+        public static bool ProcessConditionalRegionsInFile(string fileName, byte[] tokenBytes, byte[] replaceWithBytes)
+        {
+            bool modified;
+
+            using (var file = File.Open(fileName, FileMode.Open, FileAccess.ReadWrite))
+            {
+                using (var replacer = new TokenReplacer(file))
+                {
+                    modified = replacer.Replace(tokenBytes, replaceWithBytes);
+                }
+
+                file.Flush();
+            }
+
+            return modified;
+        }
+
         public void Dispose()
         {
             try
@@ -163,6 +225,129 @@ namespace LigerShark.TemplateBuilder.Tasks
             catch (ObjectDisposedException)
             {
             }
+        }
+
+        public bool ProcessConditionalRegion(byte[] startTokenBytes, byte[] endTokenBytes, bool isConditionSatisfied)
+        {
+            if (isConditionSatisfied)
+            {
+                bool isModified = Replace(startTokenBytes, NoBytes);
+                isModified |= Replace(endTokenBytes, NoBytes);
+                return isModified;
+            }
+
+            _scratch.Position = 0;
+            _scratch.SetLength(0);
+
+            //Setup buffer
+            const int defaultMinBufferSize = 16 * 1024 * 1024; //Try to read at least 16MB from the file at a time
+            var requiredMinBufferSize = Math.Max(startTokenBytes.Length, endTokenBytes.Length);
+            var bufferSize = Math.Max(requiredMinBufferSize, defaultMinBufferSize);
+            var modified = false;
+
+            //Reset source, get first buffer
+            _source.Position = 0;
+            var data = new byte[bufferSize];
+            var realBufferLength = _source.Read(data, 0, data.Length);
+            var bufferPosition = 0;
+            var bytesWrittenToScratchSinceLastFlush = 0;
+            var isReplacementPossible = true;
+            bool isInOmitBlock = false;
+
+            while (isReplacementPossible)
+            {
+                long moveBufferTo = -1;
+                int matchIndex;
+                byte[] searchFor = isInOmitBlock ? endTokenBytes : startTokenBytes;
+
+                //Complete match
+                if (FindPotentialMatchInBuffer(bufferPosition, searchFor, data, realBufferLength, out matchIndex))
+                {
+                    if (!isInOmitBlock)
+                    {
+                        //Write out the bytes leading to the match
+                        _scratch.Write(data, bufferPosition, matchIndex - bufferPosition);
+                        bytesWrittenToScratchSinceLastFlush += matchIndex - bufferPosition;
+                    }
+
+                    //Flip whether we're omitting the block
+                    isInOmitBlock = !isInOmitBlock;
+                    bufferPosition = matchIndex + searchFor.Length;
+                    modified = true;
+                }
+                //Incomplete match
+                else if (matchIndex > -1)
+                {
+                    //Calculate where we'd advance to (current position, less the current buffer size, plus the match index)
+                    var advanceLocation = matchIndex + _source.Position - realBufferLength;
+                    //If there are at least as many bytes left as are required to complete a token, replacement is possible, otherwise it's not
+                    isReplacementPossible = _source.Length - advanceLocation >= searchFor.Length;
+
+                    if (!isInOmitBlock)
+                    {
+                        //Write out the bytes leading to the potential match
+                        _scratch.Write(data, bufferPosition, matchIndex - bufferPosition);
+                        bytesWrittenToScratchSinceLastFlush += matchIndex - bufferPosition;
+                    }
+                    
+                    //If we could complete the match, move the buffer to the index 
+                    if (isReplacementPossible)
+                    {
+                        moveBufferTo = advanceLocation;
+                    }
+                }
+                //No match, advance the buffer
+                else
+                {
+                    if (!isInOmitBlock)
+                    {
+                        //Write out the remainder of the buffer
+                        _scratch.Write(data, bufferPosition, realBufferLength - bufferPosition);
+
+                        bytesWrittenToScratchSinceLastFlush += realBufferLength - bufferPosition;
+                    }
+
+                    moveBufferTo = _source.Position;
+                }
+
+                if (moveBufferTo > -1)
+                {
+                    _source.Position = moveBufferTo;
+                    isReplacementPossible = _source.Position != _source.Length;
+
+                    if (isReplacementPossible)
+                    {
+                        bufferPosition = 0;
+                        realBufferLength = _source.Read(data, 0, data.Length);
+                    }
+                    else
+                    {
+                        bufferPosition = realBufferLength = 0;
+                    }
+                }
+
+                if (bytesWrittenToScratchSinceLastFlush > bufferSize)
+                {
+                    _scratch.Flush();
+                    bytesWrittenToScratchSinceLastFlush = 0;
+                }
+            }
+
+            if (bufferPosition != realBufferLength)
+            {
+                _scratch.Write(data, bufferPosition, realBufferLength - bufferPosition);
+            }
+
+            if (modified)
+            {
+                _scratch.Flush();
+                _scratch.Position = 0;
+                _source.Position = 0;
+                _scratch.CopyTo(_source);
+                _source.SetLength(_scratch.Length);
+            }
+
+            return modified;
         }
 
         public bool Replace(byte[] tokenBytes, byte[] replacementBytes)
@@ -285,6 +470,18 @@ namespace LigerShark.TemplateBuilder.Tasks
             var replacementBytes = encoding.GetBytes(replacement);
 
             return Replace(tokenBytes, replacementBytes);
+        }
+
+        public bool ProcessConditionalRegion(string startToken, string endToken, bool isConditionSatisfied)
+        {
+            // Console.WriteLine("{0} region {1} -> {2}...", isConditionSatisfied ? "Showing": "Hiding", startToken, endToken);
+            var encoding = DetectEncoding(_source);
+
+            //Get byte runs for token and replacement
+            var startTokenBytes = encoding.GetBytes(startToken);
+            var endTokenBytes = encoding.GetBytes(endToken);
+
+            return ProcessConditionalRegion(startTokenBytes, endTokenBytes, isConditionSatisfied);
         }
 
         public bool Replace(string token, string replacement, Encoding encoding)
