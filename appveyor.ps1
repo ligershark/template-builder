d6ccd8a08804367b4d7cf899e70737f12fa76939
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2016-03-19 09:52:18 -0700

    update nuget on appv

--- appveyor.ps1
+++ appveyor.ps1
@@ -1,12 +1,12 @@
 ﻿if($env:APPVEYOR_REPO_BRANCH -eq "releasetemplatebuilder"){
-    .\build.ps1 -publishTemplateBuilderToNuget
+    .\build.ps1 -publishTemplateBuilderToNuget -updateNuget
 } 
 elseif($env:APPVEYOR_REPO_BRANCH -eq "releasefilereplacer"){
-    .\build.ps1 -publishFileReplacerToNuget
+    .\build.ps1 -publishFileReplacerToNuget -updateNuget
 }
 elseif($env:APPVEYOR_REPO_BRANCH -eq "releaseallnugetpkg"){
-    .\build.ps1 -publishFileReplacerToNuget -publishFileReplacerToNuget
+    .\build.ps1 -publishFileReplacerToNuget -publishFileReplacerToNuget -updateNuget
 }
 else {
-    .\build.ps1
+    .\build.ps1 -updateNuget
 }
