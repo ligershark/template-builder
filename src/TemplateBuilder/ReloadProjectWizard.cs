402732d8da9b24def7e0a3a46697c3b20d0fcf03
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2016-06-02 09:17:12 +0100

    Added ReloadProjectWizard to reload a project.

---
+++ src/TemplateBuilder/ReloadProjectWizard.cs
@@ -0,0 +1,43 @@
+﻿namespace TemplateBuilder
+{
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.TemplateWizard;
+    using TemplateBuilder.Helpers;
+
+    public class ReloadProjectWizard : IWizard
+    {
+        #region Public Methods
+
+        public void BeforeOpeningFile(global::EnvDTE.ProjectItem projectItem)
+        {
+        }
+
+        public void ProjectFinishedGenerating(global::EnvDTE.Project project)
+        {
+            ProjectHelper.ReloadProject(project);
+        }
+
+        public void ProjectItemFinishedGenerating(global::EnvDTE.ProjectItem projectItem)
+        {
+        }
+
+        public void RunFinished()
+        {
+        }
+
+        public void RunStarted(
+            object automationObject,
+            Dictionary<string, string> replacementsDictionary,
+            WizardRunKind runKind,
+            object[] customParams)
+        {
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return true;
+        }
+
+        #endregion
+    }
+}
